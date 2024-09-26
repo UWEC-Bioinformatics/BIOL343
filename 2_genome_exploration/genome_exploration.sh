39250f47490b9dbda6c5c7e36673953289ce50f7
Brandy <CorwinBM5021@uwec.edu>
2024-09-26 16:22:23 -0500

    Finished Homeworks 2 and 3

--- 2_genome_exploration/genome_exploration.sh
+++ 2_genome_exploration/genome_exploration.sh
@@ -1,1 +1,12 @@
-echo "This is a bash script"
+cd 2_genome_exploration
+mkdir lemons
+cd lemons
+wget -nc -O lemon_genome.fa https://www.citrusgenomedb.org/citrus_downloads/Citrus_limon/C.limon_EMF-UC_v1-Alternative_genome/assembly/Climon_v1_alternative-scaffolds.fa
+wget -O - https://www.citrusgenomedb.org/citrus_downloads/Citrus_limon/C.limon_EMF-UC_v1-Alternative_genome/annotation/Climon_v1_alternative-annotation.gff | \
+    gzip -f -d > lemon_annotations.gff
+gffread lemon_annotations.gff -T -o lemon_annotations.gtf
+grep -c ">" lemon_genome.fa > num_chromosomes.txt
+grep ">" lemon_genome.fa > chromosome_names.txt
+cut -f 3 lemon_annotations.gtf | sort | uniq -c > feature_info.txt
+grep 'CL5G054352012' lemon_annotations.gtf | grep -c "CDS" > GOI_exons.txt
+samtools faidx lemon_genome.fa
