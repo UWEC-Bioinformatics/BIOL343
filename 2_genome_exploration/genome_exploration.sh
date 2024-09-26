bc4bfeadb33dbab0797d5c85d546efb8a1446557
Katherine <mccallke0364@uwec.edu>
2024-09-26 16:51:34 -0500

    hw2_3

--- 2_genome_exploration/genome_exploration.sh
+++ 2_genome_exploration/genome_exploration.sh
@@ -5,3 +5,7 @@ wget -nc -O placenta_genome.fa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/0
 gzip -d -f placenta_genome.fa.gz
 wget -O - https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_genomic.gff.gz | gzip -f -d > placenta_annotations.gff
 gffread placenta_annotations.gff -T -o placenta_annotations.gtf
+samtools faidx placenta_genome.fa
+grep -c ">" placenta_genome.fa
+grep ">" placenta_genome.fa
+grep "rna39844" placenta_annotations.gtf| grep -c "exon"
