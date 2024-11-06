19b448fbbdecbc1150806880a8386ff8db7a126d
Carter <waldhacw6865@uwec.edu>
2024-11-06 09:21:53 -0600

    hw3

--- 2_genome_exploration/genome_exploration.sh
+++ 2_genome_exploration/genome_exploration.sh
@@ -1,7 +1,8 @@
-
 cd beegenome
 wget -nc -O genome.fa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.fna.gz
 gzip -d -f genome.fa.gz
+wget -O genome_annotations.gtf.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/254/395/GCF_003254395.2_Amel_HAv3.1/GCF_003254395.2_Amel_HAv3.1_genomic.gtf.gz
+gzip -f -d genome_annotations.gtf.gz
 grep ">" --count genome.fa > week_3_hw_genome_count.txt
 grep ">" genome.fa > week_3_hw_genome_lines.txt
 grep 'XR_001705490.2' genome_annotations.gtf | cut -f 3,4 | grep 'exon' --count > week_3_hw_exon_count.txt
