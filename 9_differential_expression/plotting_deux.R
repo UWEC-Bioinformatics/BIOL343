8dbfa60e49f5858f2a13e7ac078a4a9b8fef7c63
Katherine <mccallke0364@uwec.edu>
2024-11-12 16:42:27 -0600

    hopefully this is right

--- 9_differential_expression/plotting_deux.R
+++ 9_differential_expression/plotting_deux.R
@@ -7,3 +7,35 @@ all_counts <- read_csv("/data/classes/2024/fall/biol343/course_files/counts_clea
 all_pairwise
 
 all_counts
+
+# tidy data
+
+counts_long <- all_counts |>
+    pivot_longer(-gene_id, names_to = 'sample', values_to = 'count')
+
+counts_long |> 
+    ggplot() +
+    geom_col(aes(x = sample, y = count))
+
+decent_plot <- counts_long |>
+    filter(count >0) |>
+    separate_wider_delim(cols = sample, delim = "_", names = c('tissue', 'misc'))|>
+    ggplot()+
+    geom_density(aes(x=count,color=tissue))+
+    lims(x=c(0,100))
+
+gene_of_interest <- counts_long |>
+    filter(gene_id == 'Smp_315690') |>
+    separate_wider_delim(cols = sample, delim = "_", names = c('tissue', 'misc'))|>
+    separate_wider_position(cols= misc, widths = c("age" = 2, "rep" = 1)) #allows us to choose the widths
+
+gene_of_interest |>
+    ggplot(aes(x = tissue, y = count)) +
+    # geom_boxplot() +
+
+    geom_point(aes(color = rep, shape= age), size = 7)
+
+
+#want to separate further
+
+
