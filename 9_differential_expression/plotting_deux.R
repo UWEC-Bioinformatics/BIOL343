f6ead479070d15375080a78329e0cfd9a31c4ad7
Brandy <CorwinBM5021@uwec.edu>
2024-11-12 16:42:16 -0600

    practice in R

--- 9_differential_expression/plotting_deux.R
+++ 9_differential_expression/plotting_deux.R
@@ -7,3 +7,32 @@ all_counts <- read_csv("/data/classes/2024/fall/biol343/course_files/counts_clea
 all_pairwise
 
 all_counts
+
+# tidy data
+
+?pivot_longer()
+
+counts_long <- all_counts |>
+    pivot_longer(-gene_id, names_to = 'sample', values_to = 'count')
+
+counts_long |>
+    ggplot() +
+    geom_col(aes(x = sample, y = count))
+
+gene_of_interest <- counts_long |>
+    filter(gene_id == 'Smp_315690') |>
+    separate_wider_delim(cols = sample, delim = "_", names = c('tissue', 'misc')) |>
+    separate_wider_position(cols = misc, widths = c('age' = 2, 'rep' = 1))
+
+gene_of_interest |>
+    ggplot(aes(x = tissue, y = count)) +
+    #geom_boxplot() +
+    geom_point(aes(shape = age, color = rep), size = 7)
+
+plot <- counts_long |>
+    filter(count > 0) |>
+    separate_wider_delim(cols = sample, delim = "_", names = c('tissue', 'misc')) |>
+    ggplot(aes(x = count)) +
+    geom_density(aes(color = tissue)) +
+    lims(x = c(0, 100))
+plot
