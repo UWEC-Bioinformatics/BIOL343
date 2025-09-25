529f190c24548249f465d64f7fd66f3174ac7e16
Kyle He <heky1803@uwec.edu>
2025-09-25 15:43:07 -0500

    finished homework 3

--- 2_genome_exploration/genome_exploration.sh
+++ 2_genome_exploration/genome_exploration.sh
@@ -5,3 +5,5 @@ gzip -d -f zebrafish_genome.fa.gz
 wget -O - https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/035/GCF_000002035.5_GRCz10/GCF_000002035.5_GRCz10_genomic.gtf.gz | gzip -f -d > zebrafish_annotations.gtf
 grep -c ">" zebrafish_genome.fa > num_chromosomes.txt
 grep -F ">" zebrafish_genome.fa > chromosome_headers.txt
+cut -f 3 zebrafish_annotations.gtf | sort | uniq -c > annotation_features.txt
+grep "cep97" zebrafish_annotations.gtf | grep "XR_001800919.1" | cut -f 3 | grep -c "exon" > num_exons_gene.txt
