562a73de560003c94faa39ecf52e3e4341c9f514
Gracie <carltoga2473@uwec.edu>
2026-09-22 16:45:24 -0500

    temp

---
+++ 2_genome_exploration/genome_exploration.sh
@@ -0,0 +1,3 @@
+# copy and paste commands from previous homework into shell script
+
+# run bash genome_exploration.sh and it should run the same as the jupyter notebook
---
+++ 3_scripting/hi.sh
@@ -0,0 +1,2 @@
+echo "hello, world! (from a script)"
+python -c "print(2+2)"
